5bede3851d06add71e77cdb22626c9a2871bcb9e
Kevin Jain <thatskevinjain@gmail.com>
2023-06-07 15:16:21 -0400

    Added Submit Button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,14 +9,17 @@
 	});
 </script>
 
-<div class="main">
-    {#if visible}
-        <h1 transition:fly={{ y: 30, duration: 1000 }}>Science of Happiness</h1>
+{#if visible}
+    <div class="main" transition:fly={{ y: 30, duration: 1000 }}>
+        <h1 >Science of Happiness</h1>
+        <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint </h3>
         <div>
-            <input transition:fly={{ y: 80, duration: 1000 }} value={email} type="email"/>
+            <input class="email-input" value={email} type="email"/>
+            <button class="submit-btn" >➡️</button>
         </div>
-    {/if}
-</div>
+    </div>
+{/if}
+
 
 <img src="happiness1.jpg" alt="hello" class="background"/>
 
@@ -52,19 +55,36 @@
         text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
     }
 
-    input {
+    /* Wrap the text */
+    h3 {
+        color: #ffffff;
+        text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+        width: 80%;
+        text-align: center;
+        margin-bottom: 40px;
+    }
+
+    .submit-btn {
         font-weight: 500;
         font-size: 24px;
         line-height: 48px;
         color: #ffffff;
         text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-        background: #b0b0b0;
+        background: transparent;
         text-align: center;
-        border: 0.5px solid #ffffff;
-        box-sizing: border-box;
+        border-radius: 4px;
+        opacity: 0.8;
+    }
+
+    .email-input {
+        font-weight: 500;
+        font-size: 24px;
+        line-height: 48px;
+        color: #ffffff;
+        text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+        background: #b7b7b7;
+        text-align: center;
         border-radius: 10px;
-        padding: 10px;
-        margin-top: 20px;
         opacity: 0.8;
     }
 
