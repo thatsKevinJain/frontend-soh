394b955ec6b4cc1ebe6d3144c1cff51640cd80e2
Kevin Jain <thatskevinjain@gmail.com>
2023-06-27 16:44:50 -0400

    added clouds svg -- beautified questions

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -30,7 +30,6 @@
         top: 0;
         width: 100%;
         height: 100%;
-        object-fit: cover;
         z-index: -1;
     }
 
--- src/routes/questions/+page.svelte
+++ src/routes/questions/+page.svelte
@@ -4,40 +4,99 @@
     export let data;
 </script>
 
+<section>
+  <div class='air air1'></div>
+  <div class='air air2'></div>
+  <div class='air air3'></div>
+  <div class='air air4'></div>
+</section>
+
 <div class="main">
     <form method="POST" action="/questions?i={parseInt(data.index)+1}">
 
         <!-- Populate options for a SINGLE question -->
         {#if data.game.questions[data.index-1]?.question}
-            <p>{data.index}. {data.game.questions[data.index-1].question}</p>
-            {#each data.game.questions[data.index-1].options as o, j}
-                <label>
-                    <input  type     = {data.game.questions[data.index-1].type}
-                            name     = {data.game.questions[data.index-1].id}
-                            value    = {o.id}
-                            required = {data.game.questions[data.index-1].required ? true : null}/>
-                    {o.option}
-                </label><br>
-            {/each}     
+            <div class="block-single">
+                <p class="question-single">{data.index}. {data.game.questions[data.index-1].question}</p>
+                <div class="options-single">
+                    {#each data.game.questions[data.index-1].options as o, j}
+                        <label class="option">
+                            <input  type     = {data.game.questions[data.index-1].type}
+                                    name     = {data.game.questions[data.index-1].id}
+                                    value    = {o.id}
+                                    required = {data.game.questions[data.index-1].required ? true : null}/>
+                            {o.option}
+                        </label><br>
+                    {/each}
+                </div>
+            </div>
         {/if}
 
         <!-- Populate options for MULTIPLE questions -->
         {#if data.game.questions[data.index-1]?.questions}
-            <p>{data.index}. </p>
-            {#each data.game.questions[data.index-1]?.questions as question, i}
-                <p>{question}</p>
-                {#each data.game.questions[data.index-1].options as o, j}
-                    <label>
-                        <input  type     = {data.game.questions[data.index-1].type}
-                                value    = {parseInt(j+1)}
-                                name     = {data.game.questions[data.index-1].id + "-" + parseInt(i+1)}
-                                required = {data.game.questions[data.index-1].required ? true : null}/>
-                        {o.option}
-                    </label><br>
-                {/each} 
-            {/each}
+            <div class="block-multiple">
+                <p>{data.index}. </p>
+                {#each data.game.questions[data.index-1]?.questions as question, i}
+                    <p class="question-multiple">{question}</p>
+                    <div class="options-multiple">
+                        {#each data.game.questions[data.index-1].options as o, j}
+                            <label class="option">
+                                <input  type     = {data.game.questions[data.index-1].type}
+                                        value    = {parseInt(j+1)}
+                                        name     = {data.game.questions[data.index-1].id + "-" + parseInt(i+1)}
+                                        required = {data.game.questions[data.index-1].required ? true : null}/>
+                                {o.option}
+                            </label><br>
+                        {/each}
+                    </div>
+                {/each}
+            </div>
         {/if}
 
-        <button class="submit-btn">Submit</button>
+        <button class="submit">Submit</button>
     </form>
 </div>
+
+<style>
+
+    form {
+        width: 60%;
+        display: contents;
+    }
+
+    .option {
+        padding: 10px;
+        font-size: 18px;
+    }
+
+    .options-single {
+        display: grid;
+        justify-content: center;
+    }
+
+    .question-single {
+        font-size: 20px;
+        display: grid;
+        justify-content: center;
+    }
+
+    .submit {
+        width: 99px;
+        height: 50px;
+        background: #6750A4;
+        border-radius: 100px;
+        margin: 20px;
+        color: white;
+    }
+
+    .options-multiple {
+        display: block ruby;
+    }
+
+    .question-multiple {
+        margin-top: 40px;
+        font-size: 20px;
+        display: grid;
+    }
+
+</style>
