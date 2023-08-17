e5f6b920ac8d8e7fb98a46fe2295d68c7ae9b404
Kevin Jain <thatskevinjain@gmail.com>
2023-08-16 22:34:21 -0400

    vite ssr removed

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-	ssr: {
-		noExternal: ['three']
+	build: {
+		rollupOptions: {
+			external: ['three']
+		}
 	}
 });
